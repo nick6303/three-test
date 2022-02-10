b84626d412ef6150e90d9a84adf33ac04aa13c6e
nick <nick6303@gmail.com>
2022-02-10 18:08:38 +0800

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,74 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <div class="hello" ref="hello"></div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { defineComponent, onMounted, ref } from 'vue'
+import * as THREE from 'three'
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+export default defineComponent({
+  name: 'HelloWorld',
+  setup() {
+    const hello = ref(null)
+
+    let camera, scene, renderer, cube
+
+    const init = () => {
+      scene = new THREE.Scene()
+
+      renderer = new THREE.WebGLRenderer()
+      renderer.setSize(window.innerWidth, window.innerHeight) // 場景大小
+      renderer.setClearColor(0xeeeeee, 1.0) // 預設背景顏色
+      renderer.shadowMap.enable = true // 陰影效果
+
+      // 將渲染器的 DOM 綁到網頁上
+      hello.value.appendChild(renderer.domElement)
+
+      camera = new THREE.PerspectiveCamera(
+        45,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        100
+      )
+      camera.position.set(10, 10, 10) // 相機位置
+      camera.lookAt(scene.position) // 相機焦點
+
+      let pointLight = new THREE.PointLight(0xffffff)
+      pointLight.position.set(10, 10, -10)
+      scene.add(pointLight)
+
+      const geometry = new THREE.BoxGeometry(1, 1, 1) // 幾何體
+      const material = new THREE.MeshPhongMaterial({
+        color: 0x0000ff,
+      }) // 材質
+
+      cube = new THREE.Mesh(geometry, material) // 建立網格物件
+      cube.position.set(0, 0, 0)
+      scene.add(cube)
+    }
+
+    function animate() {
+      cube.rotation.x += 0.01
+      cube.rotation.y += 0.01
+    }
+
+    function render() {
+      animate()
+      requestAnimationFrame(render)
+      renderer.render(scene, camera)
+    }
+
+    onMounted(() => {
+      init()
+      render()
+      // 監聽螢幕寬高變化來做簡單 RWD 設定
+      window.addEventListener('resize', function () {
+        camera.aspect = window.innerWidth / window.innerHeight
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+      })
+    })
+    return { hello }
+  },
+})
+</script>
